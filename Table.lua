4f1df4e55de35a1120c1be9e4421ad6b35b9ae02
setrawmetatable <scarary1337@gmail.com>
2026-06-03 10:59:16 +0800

    Update Table.lua

--- Table.lua
+++ Table.lua
@@ -3,7 +3,7 @@ local table = {
         name = "Rost",
         http = "https://raw.githubusercontent.com/xxx14445551x/scripts/refs/heads/main/kat.lua",
         img = "rbxassetid://123584685652531"
-    }
+    },
     {
         name = "KAT",
         http = "",
