15a1162504de06b0600fa60af2139a6aef2d83a1
setrawmetatable <scarary1337@gmail.com>
2026-08-17 21:18:15 +0800

    Update Tables.lua

--- Tables.lua
+++ Tables.lua
@@ -5,10 +5,12 @@ local _V = {
   },
   Scripts = {
     {
-
+      Name = "Rost Alpha",
+      Load = "https://raw.githubusercontent.com/setrawmetatable/Scripts/refs/heads/main/Project%20/Rost.lua"
     },
     {
-
+      Name = "KAT",
+      Load = "https://raw.githubusercontent.com/setrawmetatable/Scripts/refs/heads/main/Project%20/KAT.lua"
     },
   },
 }
