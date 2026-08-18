f91f015e8d38204c2b606fd4b3ccb46d346b937e
setrawmetatable <scarary1337@gmail.com>
2026-08-18 15:08:16 +0800

    Update Tables.lua

--- Tables.lua
+++ Tables.lua
@@ -8,7 +8,7 @@ local _V = {
   Scripts = {
     {
       Name = "Rost Alpha",
-      Load = "https://raw.githubusercontent.com/setrawmetatable/Scripts/refs/heads/main/Project/Rost.lua",
+      Load = "https://api.getpolsec.com/scripts/hosted/14cca0c04b6f4664926427318ab5aacfe5f0813f4864ed96d3a96c07fd74b6db.lua",
       Time = "Updated: 2026/08/18 03:00 AM",
       Stat = "Udt"
     },
